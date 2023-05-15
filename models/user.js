0e410cb778e4f4e90836929e98f23849f27caed4
Vytautas <vytautas@Vytautass-MacBook-Pro.local>
2023-05-15 18:27:54 +0200

    first commit

---
+++ models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: {
+        type: 'string',
+        unique: [true, 'Email already exists'],
+        required: [true, 'Email is required']
+    },
+    username: {
+        type: 'string',
+        required: [true, 'Username is required'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, 'Username must be between 8 and 20 characters long and can only contain letters, numbers, underscores, and periods']
+    },
+    image: {
+        type: 'string',
+    }
+})
+
+export const User = models.User || model('User', UserSchema);
