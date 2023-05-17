0dfaba73a6d6639ebe5f969f9c249bcce96c761b
Vytautas <vytautas@Vytautass-MacBook-Pro.local>
2023-05-17 20:08:59 +0200

    Tuto done

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,6 +6,7 @@ const PromptCardList = ({ data, handleTagClick}) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
+        
         <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
       ))}
     </div>
@@ -43,7 +44,7 @@ const Feed = () => {
   </form>
 
   <PromptCardList
-    data={[allPosts]}
+    data={allPosts}
     handleTagClick={()=> {}} />
 
  </section>
